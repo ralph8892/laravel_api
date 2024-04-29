bc4d127a0c85a24bd2286b4c8163bc8fab5c8276
ralph8892 <brufalralph@gmail.com>
2024-04-30 00:48:52 +0800

    Created an endpoint on custom API for logging off users

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,4 +10,8 @@ Route::get('/', function () {
 
 Route::prefix('auth')->group(function () {
     Route::post('/login', LoginController::class);
+});
+
+Route::prefix('auth')->group(function () {
+    Route::post('/logout', LogoutController::class);
 });
