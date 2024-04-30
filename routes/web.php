9f6ad11305699497ca0ee463d07f43b745a4b725
ralph8892 <brufalralph@gmail.com>
2024-05-01 00:00:17 +0800

    Created an endpoint for registering users

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,8 +11,10 @@ Route::get('/', function () {
 
 Route::prefix('auth')->group(function () {
     Route::post('/login', LoginController::class);
+    Route::post('/logout', LogoutController::class);
+    Route::post('/register', RegisterController::class);
 });
 
 Route::prefix('auth')->group(function () {
-    Route::post('/logout', LogoutController::class);
+    
 });
