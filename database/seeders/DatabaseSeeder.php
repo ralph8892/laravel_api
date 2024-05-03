0cee886e3976160a3812ca963525f4f15e5ae4f6
ralph8892 <brufalralph@gmail.com>
2024-05-03 23:59:16 +0800

    Linking users to other resources

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Task;
+use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,8 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-       Task::factory(10)->create();
+        User::factory(5)->has(
+            Task::factory(10)
+        )->create();
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,11 +8,8 @@ use App\Http\Controllers\Api\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-Route::middleware('auth:sanctum')->prefix('v1')->group(function() {
-    Route::apiResource('/tasks', TaskController::class);
-    Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
-});
+require __DIR__ . '/api/v1.php';
+require __DIR__ . '/api/v2.php';
 
 Route::prefix('auth')->group(function () {
     Route::post('/login', LoginController::class);
