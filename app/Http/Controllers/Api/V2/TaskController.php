46ad9152d1555f3b6dc671110eec007a13c308ec
ralph8892 <brufalralph@gmail.com>
2024-05-05 01:35:21 +0800

    Fixed error code 500 internal server error response on V2 API endpoint

--- app/Http/Controllers/Api/V2/TaskController.php
+++ app/Http/Controllers/Api/V2/TaskController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
 use App\Http\Resources\TaskResource;
+use Illuminate\Support\Facades\Gate;
 
 class TaskController extends Controller
 {
@@ -33,7 +34,7 @@ class TaskController extends Controller
      */
     public function show(Task $task)
     {
-        $this->authorize('view', $task);
+        Gate::authorize('view', $task);
         return TaskResource::make($task);
     }
 
